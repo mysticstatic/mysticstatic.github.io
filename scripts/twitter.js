069186e17c030b06ce07a52a2458b14258d72e66
DmitryPustovit <Dmitryp222@gmail.com>
2014-07-28 07:25:36 -0700

    Disabled Domian

---
+++ scripts/twitter.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function() {
+    // set your twitter id
+    var user = 'dmitry222';
+     
+    // using jquery built in get json method with twitter api, return only one result
+    $.getJSON('http://twitter.com/statuses/user_timeline.json?screen_name=' + user + '&count=1&callback=?', function(data)         {
+         
+        // result returned
+        var tweet = data[0].text;
+     
+        // process links and reply
+        tweet = tweet.replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig, function(url) {
+            return ''+url+'';
+        }).replace(/B@([_a-z0-9]+)/ig, function(reply) {
+            return  reply.charAt(0)+''+reply.substring(1)+'';
+        });
+     
+        // output the result
+        $("#tweet").html(tweet);
+    }); 
+     
+});
