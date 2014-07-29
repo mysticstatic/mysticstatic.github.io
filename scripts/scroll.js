a42a991849a27256378ec3651b5fd1d8dab65732
Dmitry Pustovit <me@dmitrypustovit.com>
2014-07-29 02:16:39 -0400

    massive update

---
+++ scripts/scroll.js
@@ -0,0 +1,42 @@
+﻿function slide()
+{
+	jQuery.fn.extend(
+	{
+		scrollTo: function (speed, easing) 
+		{
+			return this.each(function () 
+			{
+				var targetOffset = $(this).offset().top;
+				$('html,body').animate({ scrollTop: targetOffset }, speed, easing);
+			});
+		}
+	});
+}
+
+function slide1()
+{
+	slide();
+
+	$('#nav').scrollTo(500);
+}
+
+function slide2()
+{
+	slide();
+
+	$('#News').scrollTo(500);
+}
+
+function slide3()
+{
+	slide();
+
+	$('#About').scrollTo(500);
+}
+
+function slide4()
+{
+	slide();
+
+	$('#Team').scrollTo(500);
+}
